91783a8cbfae23bdfc35b2013e8631a3f61b3abb
lyret <viktor@lyresten.se>
2016-11-09 20:10:35 +0100

    some stuff

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 
-//Test that Pixi is working
-console.log(PIXI);
+//Create the renderer
+var renderer = PIXI.autoDetectRenderer(600, 480, {antialias: false, transparent: false, resolution: 1});
+//renderer.backgroundColor = 0x061639;
+
+//Add the canvas to the HTML document
+document.body.appendChild(renderer.view);
+
+//Create a container object called the `stage`
+var stage = new PIXI.Container();
+
+PIXI.loader.add("images/sprite.png").load(() => {
+    var sprite = new PIXI.Sprite(PIXI.loader.resources["images/sprite.png"].texture);
+    sprite.visible = false;
+    stage.addChild(sprite);
+    renderer.render(stage);
+    setTimeout(() => sprite.visible = false, 2000)
+});
