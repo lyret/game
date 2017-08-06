9c2ea8db0f429a20aa9c91768429ea4a5389bab3
lyret <viktor@lyresten.se>
2017-08-06 23:16:06 +0200

    move to ts :)

---
+++ index.tsx
@@ -0,0 +1,144 @@
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
+import { Loop, Stage, Sprite } from 'react-game-kit'
+import { Component } from './component'
+
+class Character extends Component
+{
+    update() {
+        console.log("test")
+    }
+    render() {
+        return (
+            <Sprite
+                src="images/sprite.png"
+            />
+        );
+    }
+}
+
+class Game extends React.Component<{}, {}>
+{
+    render() {
+        return (
+            <Loop>
+                <p>Test 4</p>
+                <Stage width={1024} height={576}>
+                    <Component/>
+                </Stage>
+            </Loop>
+        );
+    }
+}
+
+
+// Render the react tree to the HTML document
+ReactDom.render(<Game/>, document.getElementById("game"));
+
+
+// class Player {
+
+//     constructor(position, sprite){
+//         this.position = position;
+//         this.sprite = sprite;
+
+//         this.vx = 0;
+//         this.vy = 0;
+//         this.moveSpeed = 10;
+
+//         this.keyW = keyboard(87);
+//         this.keyA = keyboard(65);
+//         this.keyS = keyboard(83);
+//         this.keyD = keyboard(68);
+//     }
+
+//     update(){
+
+//         this.keyW.press = () => { this.vy -= this.moveSpeed};
+//         this.keyS.press = () => { this.vy += this.moveSpeed};
+//         this.keyA.press = () => { this.vx -= this.moveSpeed};
+//         this.keyD.press = () => { this.vx += this.moveSpeed};
+
+//         this.keyW.release = () => { this.vy = 0};
+//         this.keyS.release = () => { this.vy = 0};
+//         this.keyA.release = () => { this.vx = 0};
+//         this.keyD.release = () => { this.vx = 0};
+
+//         this.position.x += this.vx;
+//         this.position.y += this.vy;
+//         this.sprite.position = this.position;
+//     }
+// }
+
+
+// var path = Loader.addPath("images/sprite.png");
+// var player;
+
+// Loader.loadResources(() => {
+//     console.log('here!');
+//     var sprite = new PIXI.Sprite(PIXI.loader.resources["images/sprite.png"].texture);
+//     var playerSprite = new PIXI.Sprite(PIXI.loader.resources["images/sprite.png"].texture);
+
+//     player = new Player({x:100, y:100}, playerSprite);
+
+//     //sprite.visible = false;
+
+//     stage.addChild(sprite);
+//     stage.addChild(player.sprite);
+
+//     gameLoop();
+// });
+// /*
+// function setup(){
+//     console.log("setup");
+//     gameLoop();
+// }
+// */
+// function gameLoop(){
+//     requestAnimationFrame(gameLoop);
+
+//     player.update(); // FIX: for each gameobject update
+
+//     renderer.render(stage);
+// }
+
+
+
+
+
+// function keyboard(keyCode) {
+//   var key = {};
+//   key.code = keyCode;
+//   key.isDown = false;
+//   key.isUp = true;
+//   key.press = undefined;
+//   key.release = undefined;
+//   //The `downHandler`
+//   key.downHandler = function(event) {
+//     if (event.keyCode === key.code) {
+//       if (key.isUp && key.press) key.press();
+//       key.isDown = true;
+//       key.isUp = false;
+//     }
+//     event.preventDefault();
+//   };
+
+//   //The `upHandler`
+//   key.upHandler = function(event) {
+//     if (event.keyCode === key.code) {
+//       if (key.isDown && key.release) key.release();
+//       key.isDown = false;
+//       key.isUp = true;
+//     }
+//     event.preventDefault();
+//   };
+
+//   //Attach event listeners
+//   window.addEventListener(
+//     "keydown", key.downHandler.bind(key), false
+//   );
+//   window.addEventListener(
+//     "keyup", key.upHandler.bind(key), false
+//   );
+//   return key;
+// }
